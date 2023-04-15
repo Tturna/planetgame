0c6b37edac9bd4be17931748b8a3c9f6295ab098
Tturna <42873350+Tturna@users.noreply.github.com>
2023-04-15 11:36:16 +0300

    rotated terrain pixels + bullshit grass

--- Assets/Scripts/CameraScripts/TerrainCameraController.cs
+++ Assets/Scripts/CameraScripts/TerrainCameraController.cs
@@ -4,13 +4,19 @@ namespace CameraScripts
 {
     public class TerrainCameraController : MonoBehaviour
     {
+        [SerializeField] private RectTransform terrainRender;
+        
         private Camera mainCam;
 
         private void LateUpdate()
         {
             mainCam ??= Camera.main;
 
-            transform.position = mainCam.transform.position;
+            transform.position = mainCam!.transform.position;
+            
+            // prevent rotation
+            transform.rotation = Quaternion.identity;
+            terrainRender.rotation = Quaternion.identity;
         }
     }
 }
