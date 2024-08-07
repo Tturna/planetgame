e294b9dfc1759ff538be7c5f661ebd20311e8037
Tturna <42873350+Tturna@users.noreply.github.com>
2024-08-07 18:12:36 +0300

    placement utility refactor and CTRL orients rooms to planet

--- Assets/Scripts/Inventory/PlaceableUtility.cs
+++ Assets/Scripts/Inventory/PlaceableUtility.cs
@@ -20,6 +20,96 @@ namespace Inventory
             _initialized = true;
         }
 
+        private static (Collider2D, Transform) GetClosestNeighbor(Vector3 mousePoint, int neighborCount)
+        {
+            var closestNeighbor = _neighbors[0];
+            var closestRoomTransform = closestNeighbor.transform.root;
+            var closestPoint = closestNeighbor.ClosestPoint(mousePoint);
+
+            for (var i = 1; i < neighborCount; i++)
+            {
+                var neighbor = _neighbors[i];
+                if (neighbor == closestNeighbor) continue;
+                if (neighbor.transform.root == closestRoomTransform) continue;
+                
+                var neighborClosestPoint = neighbor.ClosestPoint(mousePoint);
+                
+                if (Vector2.Distance(mousePoint, neighborClosestPoint) < Vector2.Distance(mousePoint, closestPoint))
+                {
+                    closestNeighbor = neighbor;
+                    closestRoomTransform = closestNeighbor.transform.root;
+                    closestPoint = neighborClosestPoint;
+                }
+            }
+            
+            return (closestNeighbor, closestRoomTransform);
+        }
+
+        private static bool TryGetStackedBuildingPosition(RoomModuleSo roomModuleSo,
+            Vector3 mousePoint,
+            int neighborCount,
+            int buildingBoundsMask,
+            out Vector3? position,
+            out Vector3? normal)
+        {
+            var (closestNeighbor, closestRoomTransform) = GetClosestNeighbor(mousePoint, neighborCount);
+            
+            var closestNeighborPos = closestRoomTransform.position;
+            var closestNeighborUp = closestRoomTransform.up;
+            var closestNeighborRight = closestRoomTransform.right;
+            
+            var neighborToMouse = mousePoint - closestNeighborPos;
+            var relativeMouse = closestRoomTransform.InverseTransformDirection(neighborToMouse);
+            var neighborSize = ((BoxCollider2D)closestNeighbor).size;
+            var compareVertical = Mathf.Abs(relativeMouse.y) > Mathf.Abs(relativeMouse.x);
+
+            var offsetDirection = relativeMouse switch
+            {
+                _ when !compareVertical && relativeMouse.x > 0f => closestNeighborRight,
+                _ when !compareVertical && relativeMouse.x < 0f => -closestNeighborRight,
+                _ when relativeMouse.y > 0f => closestNeighborUp,
+                _ when relativeMouse.y < 0f => -closestNeighborUp,
+                _ => throw new ArgumentOutOfRangeException()
+            };
+            
+            var offset = compareVertical
+                ? offsetDirection * roomModuleSo.boundsSize.y / 2f
+                : offsetDirection * roomModuleSo.boundsSize.x / 2f;
+            
+            var neighborBoundExtent = (compareVertical ? neighborSize.y : neighborSize.x) / 2f;
+            normal = closestNeighborUp;
+            
+            // move down by half the room size because HeldItemManager and PlaceableLogic where this data
+            // is used will move the object up by half the room size. They do this because they are made for
+            // generic placeable objects that need this behavior, as do rooms when they're not
+            // connected to neighbors.
+            position = closestNeighborPos + offset + offsetDirection * neighborBoundExtent - normal * roomModuleSo.boundsSize.y / 2;
+
+            if (compareVertical)
+            {
+                return true;
+            }
+            
+            const int maxHorizontalStack = 2;
+            const float offsetAddition = 0.2f;
+            var checkpoint = (Vector3)position;
+            var pointOffset = -offset.normalized * (roomModuleSo.boundsSize.x / 2f + offsetAddition);
+
+            for (var i = 0; i <= maxHorizontalStack; i++)
+            {
+                var hit = Physics2D.OverlapPoint(checkpoint + pointOffset, buildingBoundsMask);
+
+                if (hit)
+                {
+                    checkpoint = hit.transform.position;
+                    pointOffset = -offset.normalized * (hit.bounds.size.x / 2f + offsetAddition);
+                }
+                else return true;
+            }
+            
+            return false;
+        }
+
         public static bool TryGetPlaceablePosition(Vector3 mousePoint, ItemSo equippedItemSo, out Vector3? position, out Vector3? normal)
         {
             if (!_initialized) Init();
@@ -32,82 +122,10 @@ namespace Inventory
                 var buildingBoundsMask = 1 << LayerMask.NameToLayer("BuildingBounds");
                 var neighborCheckRange = Mathf.Max(roomModuleSo.boundsSize.x, roomModuleSo.boundsSize.y);
                 var neighborCount = Physics2D.OverlapCircleNonAlloc(mousePoint, neighborCheckRange, _neighbors, buildingBoundsMask);
-                
+
                 if (neighborCount > 0)
                 {
-                    var closestNeighbor = _neighbors[0];
-                    var closestRoomTransform = closestNeighbor.transform.root;
-                    var closestPoint = closestNeighbor.ClosestPoint(mousePoint);
-
-                    for (var i = 1; i < neighborCount; i++)
-                    {
-                        var neighbor = _neighbors[i];
-                        if (neighbor == closestNeighbor) continue;
-                        if (neighbor.transform.root == closestRoomTransform) continue;
-                        
-                        var neighborClosestPoint = neighbor.ClosestPoint(mousePoint);
-                        
-                        if (Vector2.Distance(mousePoint, neighborClosestPoint) < Vector2.Distance(mousePoint, closestPoint))
-                        {
-                            closestNeighbor = neighbor;
-                            closestRoomTransform = closestNeighbor.transform.root;
-                            closestPoint = neighborClosestPoint;
-                        }
-                    }
-                    
-                    var closestNeighborPos = closestRoomTransform.position;
-                    var closestNeighborUp = closestRoomTransform.up;
-                    var closestNeighborRight = closestRoomTransform.right;
-                    var neighborToMouse = mousePoint - closestNeighborPos;
-                    var relativeMouse = closestRoomTransform.InverseTransformDirection(neighborToMouse);
-                    var compareVertical = Mathf.Abs(relativeMouse.y) > Mathf.Abs(relativeMouse.x);
-                    var neighborSize = ((BoxCollider2D)closestNeighbor).size;
-
-                    var offsetDirection = relativeMouse switch
-                    {
-                        _ when !compareVertical && relativeMouse.x > 0f => closestNeighborRight,
-                        _ when !compareVertical && relativeMouse.x < 0f => -closestNeighborRight,
-                        _ when relativeMouse.y > 0f => closestNeighborUp,
-                        _ when relativeMouse.y < 0f => -closestNeighborUp,
-                        _ => throw new ArgumentOutOfRangeException()
-                    };
-                    
-                    var offset = compareVertical
-                        ? offsetDirection * roomModuleSo.boundsSize.y / 2f
-                        : offsetDirection * roomModuleSo.boundsSize.x / 2f;
-                    
-                    var neighborBoundExtent = (compareVertical ? neighborSize.y : neighborSize.x) / 2f;
-                    normal = closestNeighborUp;
-                    // move down by half the room size because HeldItemManager and PlaceableLogic where this data
-                    // is used will move the object up by half the room size. They do this because they are made for
-                    // generic placeable objects that need this behavior, as do rooms when they're not
-                    // connected to neighbors.
-                    
-                    position = closestNeighborPos + offset + offsetDirection * neighborBoundExtent - normal * roomModuleSo.boundsSize.y / 2;
-
-                    if (compareVertical)
-                    {
-                        return true;
-                    }
-                    
-                    const int maxHorizontalStack = 2;
-                    const float offsetAddition = 0.2f;
-                    var checkpoint = (Vector3)position;
-                    var pointOffset = -offset.normalized * (roomModuleSo.boundsSize.x / 2f + offsetAddition);
-
-                    for (var i = 0; i <= maxHorizontalStack; i++)
-                    {
-                        var hit = Physics2D.OverlapPoint(checkpoint + pointOffset, buildingBoundsMask);
-
-                        if (hit)
-                        {
-                            checkpoint = hit.transform.position;
-                            pointOffset = -offset.normalized * (hit.bounds.size.x / 2f + offsetAddition);
-                        }
-                        else return true;
-                    }
-                    
-                    return false;
+                    return TryGetStackedBuildingPosition(roomModuleSo, mousePoint, neighborCount, buildingBoundsMask, out position, out normal);
                 }
             }
             
@@ -120,12 +138,14 @@ namespace Inventory
                 return false;
             }
 
-            var rayHit = Physics2D.Raycast(mousePoint, PlayerController.instance.DirectionToClosestPlanet, placementAssistRange, mask);
+            var planetDirection = PlayerController.instance.DirectionToClosestPlanet;
+            var rayHit = Physics2D.Raycast(mousePoint, planetDirection, placementAssistRange, mask);
+            var alt = Input.GetKey(KeyCode.LeftControl) || Input.GetKey(KeyCode.RightControl);
 
             if (rayHit)
             {
                 position = rayHit.point;
-                normal = rayHit.normal;
+                normal = alt ? -planetDirection : rayHit.normal;
             }
             else
             {
@@ -145,7 +165,7 @@ namespace Inventory
                 }
                 
                 position = closestCollider.ClosestPoint(mousePoint);
-                normal = (mousePoint - (Vector3)position).normalized;
+                normal = alt ? -planetDirection : (mousePoint - (Vector3)position).normalized;
             }
 
             // Prevent placing item inside the terrain
