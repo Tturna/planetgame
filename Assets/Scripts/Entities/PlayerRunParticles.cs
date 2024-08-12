c68717ecde064498b525c2249a95ff07b17d5a0a
Tturna <42873350+Tturna@users.noreply.github.com>
2024-08-12 15:37:38 +0300

    better run particles

--- Assets/Scripts/Entities/PlayerRunParticles.cs
+++ Assets/Scripts/Entities/PlayerRunParticles.cs
@@ -1,4 +1,3 @@
-using System;
 using UnityEngine;
 
 namespace Entities
@@ -6,25 +5,29 @@ namespace Entities
     public class PlayerRunParticles : MonoBehaviour
     {
         [SerializeField] private float maxEmissionRate;
+        [SerializeField] private float positionOffset;
         
+        private PlayerController _player;
         private Rigidbody2D _playerRb;
-        private ParticleSystem _runPs;
-        private ParticleSystem.EmissionModule _runPsEmission;
+        private ParticleSystem _runPfx;
+        private ParticleSystem.EmissionModule _runPfxEmission;
+        private Transform _runPfxTransform;
 
         private bool _canEmit;
         
         private void Start()
         {
-            var player = PlayerController.instance;
-            _playerRb = player.GetComponent<Rigidbody2D>();
-            _runPs = GetComponent<ParticleSystem>();
-            _runPsEmission = _runPs.emission;
+            _player = PlayerController.instance;
+            _playerRb = _player.GetComponent<Rigidbody2D>();
+            _runPfx = GetComponent<ParticleSystem>();
+            _runPfxEmission = _runPfx.emission;
+            _runPfxTransform = _runPfx.transform;
             
-            player.Jumped += OnJump;
-            player.Grounded += OnGrounded;
+            _player.Jumped += OnJump;
+            _player.Grounded += OnGrounded;
             
             _canEmit = true;
-            _runPs.Play();
+            _runPfx.Play();
         }
 
         private void OnDestroy()
@@ -39,17 +42,20 @@ namespace Entities
         {
             if (!_canEmit) return;
             var velocity = _playerRb.velocity.magnitude;
-            // Debug.Log($"Velocity: {velocity}");
-
             var nVel = velocity / MaxVelocity;
             var emissionRate = Mathf.Lerp(0, maxEmissionRate, nVel);
-            _runPsEmission.rateOverTime = emissionRate;
+            _runPfxEmission.rateOverTime = emissionRate;
+            
+            var runDirection = _player.GetInputVector().x > 0 ? -1 : 1;
+            var position = _runPfxTransform.localPosition;
+            position.x = runDirection * positionOffset;
+            _runPfxTransform.localPosition = position;
         }
         
         private void OnJump()
         {
             _canEmit = false;
-            _runPsEmission.rateOverTime = 0f;
+            _runPfxEmission.rateOverTime = 0f;
         }
         
         private void OnGrounded()
